501d25df73420b84c8e000eeda68b16cc460efaf
Paulo Araujo <95araujopaulo@gmail.com>
2021-03-14 23:33:24 -0300

    refact to single Sonda

--- index.js
+++ index.js
@@ -7,11 +7,6 @@ let positionSonda = [{
     x: 0,
     y: 0,
     looking: "N"
-},
-{
-    x: 0,
-    y: 0,
-    looking: "N"
 }]
 
 
@@ -32,90 +27,81 @@ const rotationRight = {
 let messageToInput = "Quais são as coordenadas iniciais?"
 let sonda = 0
 let coordinatesMove = 0
-function validateMalha(sonda){
-    if(positionSonda[sonda].y > 5 || positionSonda[sonda].y < 0 ){
+function validateMalha(){
+    if(positionSonda[0].y > 5 || positionSonda[0].y < 0 ){
         console.log("Sonda saiu da malha na posição y")
         return false
     }
-    if(positionSonda[sonda].x > 5 || positionSonda[sonda].x < 0 ){
+    if(positionSonda[0].x > 5 || positionSonda[0].x < 0 ){
         console.log("Sonda saiu da malha na posição x")
         return false
     }
     return true
 }
 
-function moveFront(sonda){
-    let indexSondaMove = sonda - 1
-    let direction = positionSonda[indexSondaMove].looking
-   if(direction == 'N') positionSonda[indexSondaMove].y += 1
-   if(direction == 'S') positionSonda[indexSondaMove].y -= 1
-   if(direction == 'E') positionSonda[indexSondaMove].x += 1
-   if(direction == 'W') positionSonda[indexSondaMove].x -= 1
-   if(!validateMalha(indexSondaMove)){
-       console.log("A suas movimentações foram causaram um acidente com a sonda "+indexSondaMove)
+function moveFront(){
+    let indexSondaMove = 0
+    let direction = positionSonda[0].looking
+    positionSonda[0].y = parseInt(positionSonda[0].y)
+    positionSonda[0].x = parseInt(positionSonda[0].x)
+   if(direction == 'N') positionSonda[0].y += 1
+   if(direction == 'S') positionSonda[0].y -= 1
+   if(direction == 'E') positionSonda[0].x += 1
+   if(direction == 'W') positionSonda[0].x -= 1
+   if(!validateMalha(0)){
+       console.log("A suas movimentações causaram um acidente com a sonda")
    }
    return true
    
 }
-function rotateLeft(sonda){
-    let indexSondaLft = sonda - 1
-    let direction = positionSonda[indexSondaLft].looking
-   if(direction == 'N') positionSonda[indexSondaLft].looking = rotationLeft.N
-   if(direction == 'S') positionSonda[indexSondaLft].looking = rotationLeft.S
-   if(direction == 'E') positionSonda[indexSondaLft].looking = rotationLeft.E
-   if(direction == 'W') positionSonda[indexSondaLft].looking = rotationLeft.W
+function rotateLeft(){
+    let indexSondaLft = 0
+    let direction = positionSonda[0].looking
+   if(direction == 'N') positionSonda[0].looking = rotationLeft.N
+   if(direction == 'S') positionSonda[0].looking = rotationLeft.S
+   if(direction == 'E') positionSonda[0].looking = rotationLeft.E
+   if(direction == 'W') positionSonda[0].looking = rotationLeft.W
 }
-function rotateRight(sonda){
-    let indexSondarRgt = sonda - 1
-    let direction = positionSonda[indexSondarRgt].looking
-   if(direction == 'N') positionSonda[indexSondarRgt].looking = rotationRight.N
-   if(direction == 'S') positionSonda[indexSondarRgt].looking = rotationRight.S
-   if(direction == 'E') positionSonda[indexSondarRgt].looking = rotationRight.E
-   if(direction == 'W') positionSonda[indexSondarRgt].looking = rotationRight.W
+function rotateRight(){
+    let indexSondarRgt = 0
+    let direction = positionSonda[0].looking
+   if(direction == 'N') positionSonda[0].looking = rotationRight.N
+   if(direction == 'S') positionSonda[0].looking = rotationRight.S
+   if(direction == 'E') positionSonda[0].looking = rotationRight.E
+   if(direction == 'W') positionSonda[0].looking = rotationRight.W
 }
 
-function execMoove(commandsMove, sonda) {
+function execMoove(commandsMove) {
     commandsMove = commandsMove.toUpperCase()
     for (let i = 0; i < commandsMove.length; i++) {
         if (commandsMove[i] == 'M') {
-            moveFront(sonda)
+            moveFront()
         }
         if (commandsMove[i] == 'L') {
-            rotateLeft(sonda)
+            rotateLeft()
         }
         if (commandsMove[i] == 'R') {
-            rotateRight(sonda)
+            rotateRight()
         }
     }
 }
 
-function moveSonda(value, sonda, coordinatesMove) {
+function moveSonda(value) {
     let commandsToSonda = value.split(" ")
-    if (sonda == 1) {
         if (coordinatesMove) {
-            execMoove(value, sonda)
-            messageToInputCoordinatesToMove = "Insira as instruções de movimento da segunda Sonda!"
-            coordinatesToMove()
+            execMoove(value)
+            messageToInputCoordinatesToMove = "Insira as instruções de movimento da Sonda!"
+            console.log(positionSonda[0].x + ' ' + positionSonda[0].y + ' ' + positionSonda[0].looking)
+            coordinatesMove = 0
+            coordinates()
             return false
         }
         positionSonda[0].x = commandsToSonda[0]
         positionSonda[0].y = commandsToSonda[1]
         positionSonda[0].looking = commandsToSonda[2].toUpperCase()
-        messageToInputCoordinates = "Insira as cordenadas da segunda Sonda!"
-        coordinates()
-    } else if (sonda == 2) {
-        if (coordinatesMove) {
-            execMoove(value, sonda)
-            console.log(positionSonda[0])
-            console.log(positionSonda[1])
-            return false
-        }
-        positionSonda[1].x = commandsToSonda[0]
-        positionSonda[1].y = commandsToSonda[1]
-        positionSonda[1].looking = commandsToSonda[2].toUpperCase()
+        messageToInputCoordinates = "Insira as cordenadas da nova Sonda!"
         coordinatesToMove()
-        
-    }
+    
     return positionSonda;
 }
 function validateInputCoordinates(value) {
@@ -170,22 +156,21 @@ function initExploreMars() {
         coordinates()
     })
 }
-messageToInputCoordinates = "Insira as cordenadas da primeira Sonda!"
+messageToInputCoordinates = "Insira as cordenadas da Sonda!"
 
 function coordinates() {
-    sonda++
+    
     readline.question(messageToInputCoordinates, (value) => {
         if (!validateInputCoordinates(value)) {
             messageToInput = "Insira um novo comando!"
             return coordinates()
         }
-        moveSonda(value, sonda, coordinatesMove)
+        moveSonda(value, coordinatesMove)
     })
 }
 
-messageToInputCoordinatesToMove = "Insira as instruções de movimento da primeira Sonda!"
+messageToInputCoordinatesToMove = "Insira as instruções de movimento da Sonda!"
 function coordinatesToMove() {
-    sonda++
     coordinatesMove++
     readline.question(messageToInputCoordinatesToMove, (value) => {
         if (!validateInputCoordinatesToMove(value)) {
@@ -193,7 +178,7 @@ function coordinatesToMove() {
             return coordinatesToMove()
         }
         
-        moveSonda(value, sonda, coordinatesMove)
+        moveSonda(value, coordinatesMove)
         
     })
 }
