56bf5599dfe4b58d3c21bdf52be988cc0f6bdadd
Paulo Araujo <95araujopaulo@gmail.com>
2021-03-16 08:06:15 -0300

    refact: add design pattern BO and separating responsibilities

--- spec/marsSpec.js
+++ spec/marsSpec.js
@@ -1,1 +1,11 @@
 //init to teste de unidade
+const mars = require("../src/index")
+console.log(mars('5 5'),'mars')
+describe("Exploring Mars", function(){
+    describe("Teste Mars 1", function (){
+       it("teste do it", function(){
+
+       }) 
+    })
+
+})
